cd4623c987cdf8431e63e031495e405f6670e59b
Juan_Sebastian Molina <juan.molf16@gmail.com>
2020-04-08 17:09:51 -0500

    estilos

--- src/components/logohome.jsx
+++ src/components/logohome.jsx
@@ -8,11 +8,13 @@ const Logohome = () => {
         display: "flex",
         alignItems: "center",
         flexDirection: "column",
+        padding: "10%",
       }}
     >
       <img src={cenace} alt="" />
-      <h1>Operador Nacional de Electricidad</h1>
+      <h1 className="myh1 h1">Operador Nacional de Electricidad</h1>
       <h1>CENACE</h1>
+      <button className="myh1 btn btn-primary">Login</button>
     </div>
   );
 };
